7f31b7352c34e4fa8bc6d78be6a94f5cb2dc0d0f
Niles <nileschampeaux@gmail.com>
2022-02-09 18:27:22 -0800

    Add new function that plays 5 rounds of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!");
-
 // Project: Rock Paper Scissors
 
 // Starting with an algorithm for the game:
@@ -16,53 +14,114 @@ function computerPlay() {
 // the playerSelection parameter should be case-insensitive to account for variations
 
 // use toLowerCase() function to ensure all inputs are the same case
-let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
+// let playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
 
 // added a while loop to ensure the user enters one of the three options
-while (
-  playerSelection !== "rock" &&
-  playerSelection !== "paper" &&
-  playerSelection !== "scissors"
-) {
-  playerSelection = prompt(
-    "Choose a valid input: rock, paper, or scissors"
-  ).toLowerCase();
-}
+// while (
+//   playerSelection !== "rock" &&
+//   playerSelection !== "paper" &&
+//   playerSelection !== "scissors"
+// ) {
+//   playerSelection = prompt(
+//     "Choose a valid input: rock, paper, or scissors"
+//   ).toLowerCase();
+// }
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Paper beats rock! Sorry!`
     );
+    computerScore++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Paper beats rock! Congrats!`
     );
+    playerScore++;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Rock beats scissors! Congrats!`
     );
+    playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Rock beats scissors! Sorry!`
     );
+    computerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Scissors beats paper! Congrats!`
     );
+    playerScore++;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. Scissors beats paper! Sorry!`
     );
+    computerScore++;
   } else if (playerSelection === computerSelection) {
     console.log(
       `You chose ${playerSelection} and the computer chose ${computerSelection}. That's a tie!`
     );
   }
+  return playerScore;
+  return computerScore;
 }
 
-playRound(playerSelection, computerPlay());
+// functon playRound() plays a single round of rock, paper, scissors.
+// playRound(playerSelection, computerPlay());
 
-// write a new function called game().
+// write a new function called game()
 // insert playRound() inside game() and loop playRound() to play a 5 round game
 // Keep score and report a winner and loser at the end
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(
+      "Choose rock, paper, or scissors"
+    ).toLowerCase();
+    while (
+      playerSelection !== "rock" &&
+      playerSelection !== "paper" &&
+      playerSelection !== "scissors"
+    ) {
+      playerSelection = prompt(
+        "Choose a valid input: rock, paper, or scissors"
+      ).toLowerCase();
+    }
+    playRound(playerSelection, computerPlay());
+  }
+}
+
+// invoke function game() to play 5 rounds
+game();
+
+// report a winner at the end
+// IF number of player wins is less than player losses, print player loser
+// ELSE print player winner
+
+if (playerScore > computerScore) {
+  console.log(
+    `Congrats! You beat the computer ${playerScore} out of 5 rounds!`
+  );
+} else if (playerScore === computerScore) {
+  console.log("This game ended in a tie. How bizzare.");
+} else {
+  console.log(
+    `Oh no, the computer beat you ${computerScore} out of 5 rounds...`
+  );
+}
+
+// in all 3 result scenarios (player wins, player loses, all ties), ask player if they want to play again
+
+// I should find a way to keep the user in a while loop so they are prompted at the conclusion of every game
+
+let newGame = prompt("Would you like to play again? Choose yes or no");
+
+if (newGame === "yes") {
+  game();
+} else {
+  console.log("Ok, goodbye for now.");
+}
